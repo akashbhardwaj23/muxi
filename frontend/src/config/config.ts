71b587ffd0eae0c3874aabb39b043471b0951572
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-07-27 09:37:01 +0100

    fixed url

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,2 +1,2 @@
 
-export const BACKEND_URL = process.env.NODE_ENV === "production" ? "https://be.akashwork.tody:8080" : "http://localhost:8080"
+export const BACKEND_URL = process.env.NODE_ENV === "production" ? "https://be.akashwork.today" : "http://localhost:8080"
