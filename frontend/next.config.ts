796843b63b5f68ea8409d8f437a12e7a829d5ab8
akashbhardwaj23 <akashbhardwaj415@gmail.com>
2025-07-29 08:06:44 +0100

    fixed ci.yml

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -8,5 +8,13 @@ const nextConfig: NextConfig = {
   }
 };
 
+  console.log("Next config is ", process.env.NEXT_PUBLIC_APIKEY);
+  console.log("Next config is ", process.env.NEXT_PUBLIC_AUTHDOMAIN);
+  console.log("Next config is ", process.env.NEXT_PUBLIC_PROJECTID);
+  console.log("Next config is ", process.env.NEXT_PUBLIC_DATABASE_URL); 
+  console.log("Next config is ", process.env.NEXT_PUBLIC_STORAGE_BUCKET);
+  console.log("Next config is ", process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID);
+  console.log("Next config is ", process.env.NEXT_PUBLIC_APP_ID);
+
 
 export default nextConfig;
